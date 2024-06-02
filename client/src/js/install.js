e5dee20be469b795b79ca77278ee69f2dc46db88
jtschams <jtschams@yahoo.com>
2024-06-02 12:36:40 -0500

    display logic on install button

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -3,16 +3,16 @@ const butInstall = document.getElementById('buttonInstall');
 // Logic for installing the PWA
 window.addEventListener('beforeinstallprompt', (event) => {
   event.preventDefault();
+  butInstall.hidden = false;
 
   butInstall.addEventListener('click', async () => {
     event.prompt();
     butInstall.setAttribute('disabled', true);
-    installBtn.textContent = 'Installed'
   });
 });
 
 
 window.addEventListener('appinstalled', (event) => {
-  butInstall.setAttribute('hidden', true);
-  console.log('App already installed', event);
+  butInstall.textContent = 'Installed'
+  console.log('App installed!', event);
 });
